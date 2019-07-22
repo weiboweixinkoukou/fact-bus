80e4b46a85cda17660b7ebef5c21981550afc7e7
sxp <sxp2034113858@163.com>
2019-07-23 01:30:32 +0800

    eventbus 使用策略模式

--- src/main/java/com/woter/fact/bus/test/strategy/CallKingDeeStrategy.java
+++ src/main/java/com/woter/fact/bus/test/strategy/CallKingDeeStrategy.java
@@ -5,8 +5,12 @@ import com.woter.fact.bus.test.KingDeeBaseEvent;
 public interface CallKingDeeStrategy {
 
     String doCallKingDee(KingDeeBaseEvent kingDeeBaseEvent);
+
     /// <summary>
     /// 是否可以解析
     /// </summary>
     boolean Analysisable();
+
+    //TODO 插入失败数据
+
 }
